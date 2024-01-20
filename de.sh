beff194dee7466fac087bfa07e54e0e9f16acf09
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-01-20 03:52:44 -0700

    Update and rename dd to de.sh

--- de.sh
+++ de.sh
@@ -4,17 +4,10 @@
 sudo apt-get install -y budgie-desktop
 sudo apt install --install-suggests budgie-desktop -y
 
-sleep2
+wait
 
 # Install essential packages
 sudo apt-get install -y build-essential nano micro nala brightnessctl playerctl dpkg synaptic neofetch htop btop cava xdg-user-dirs xdg-user-dirs-gtk dialog mtools dosfstools gvfs gvfs-backends policykit-1-gnome network-manager network-manager-gnome thunar thunar-archive-plugin thunar-volman file-roller tilix alacritty terminator kitty alacritty pulseaudio alsa-utils pavucontrol volumeicon-alsa pamixer neofetch curl fonts-recommended fonts-ubuntu fonts-font-awesome fonts-terminus picom rofi dunst libnotify-bin unzip zip fonts-font-awesome fonts-ubuntu fonts-liberation2 fonts-liberation fonts-terminus geany geany-plugins pdfarranger numix-icon-theme numix-gtk-theme dmz-cursor-theme intel-microcode flatpak easyssh plymouth transmission-gtk obs-studio mkvtoolnix-gui mpv qimgv notepadqq firefox-esr redshift cpu-x udns-utils whois curl
-sddm sddm-gtk-greeter-settings
-
-sleep 2
-
-# Enable SDDM and install a theme
-sudo systemctl enable sddm
-git clone https://github.com/stuomas/delicious-sddm-theme.git && cd delicious-sddm-theme && ./install.sh
 
 wait
 
@@ -26,5 +19,15 @@ sudo systemctl enable sddm
 # Update user directory
 xdg-user-dirs-update
 
-# Love Myself
+# Install SDDM with recommended packages excluded
+sudo apt install --no-install-recommends sddm
+
+# Enable SDDM service to start at boot
+sudo systemctl enable sddm
+
+git clone https://github.com/stuomas/delicious-sddm-theme.git && cd delicious-sddm-theme && ./install.sh
+
+wait
+
+# reboot
 sudo reboot
