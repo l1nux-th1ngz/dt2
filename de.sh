c5ec7856062531268bfe36e50ae0b04e56948f70
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-01-20 05:49:48 -0700

    Update de.sh

--- de.sh
+++ de.sh
@@ -3,28 +3,11 @@
 # Install Budgie Desktop
 sudo apt-get install -y budgie-desktop
 sudo apt install --install-suggests budgie-desktop -y
-
-wait
-
-# Install essential packages (split into multiple lines for readability)
-sudo apt-get install build-essential nano micro nala brightnessctl playerctl \
-dpkg synaptic neofetch htop btop cava xdg-user-dirs xdg-user-dirs-gtk \
-dialog mtools dosfstools gvfs gvfs-backends policykit-1-gnome \
-network-manager network-manager-gnome thunar thunar-archive-plugin thunar-volman \
-file-roller tilix alacritty terminator kitty alacritty pulseaudio alsa-utils \
-pavucontrol volumeicon-alsa pamixer neofetch curl fonts-recommended \
-fonts-ubuntu fonts-font-awesome fonts-terminus picom rofi dunst libnotify-bin \
-unzip zip fonts-font-awesome fonts-ubuntu fonts-liberation2 fonts-liberation \
-fonts-terminus geany geany-plugins pdfarranger numix-icon-theme numix-gtk-theme \
-dmz-cursor-theme intel-microcode flatpak easyssh plymouth transmission-gtk \
-obs-studio mkvtoolnix-gui mpv qimgv notepadqq firefox-esr redshift cpu-x \
-udns-utils whois curl
-
-wait
-
-# Enable Flatpak and add Flathub repository
-sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo systemctl enable sddm
+sudo apt-get install -y synaptic
+sudo apt-get install -y xdg-user-dirs
+sudo apt-get install -y xdg-user-dirs-gtk
+sudo apt-get install -y kitty
+sudo apt-get install -y terminator
 
 # Update user directory
 xdg-user-dirs-update
