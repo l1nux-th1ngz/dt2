f9c69c659cbd013273c55ef9abb9fcc7170a6ba1
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-01-20 05:35:11 -0700

    Update de.sh

--- de.sh
+++ de.sh
@@ -6,22 +6,23 @@ sudo apt install --install-suggests budgie-desktop -y
 
 wait
 
-# Install essential packages
-sudo apt-get install build-essential nano micro nala brightnessctl playerctl 
-dpkg synaptic neofetch htop btop cava xdg-user-dirs xdg-user-dirs-gtk
-dialog mtools dosfstools gvfs gvfs-backends policykit-1-gnome network-manager
-network-manager-gnome thunar thunar-archive-plugin thunar-volman file-roller 
-tilix alacritty terminator kitty alacritty pulseaudio alsa-utils pavucontrol
-volumeicon-alsa pamixer neofetch curl fonts-recommended fonts-ubuntu fonts-font-awesome 
-fonts-terminus picom rofi dunst libnotify-bin unzip zip fonts-font-awesome fonts-ubuntu 
-fonts-liberation2 fonts-liberation fonts-terminus geany geany-plugins pdfarranger 
-numix-icon-theme numix-gtk-theme dmz-cursor-theme intel-microcode flatpak easyssh plymouth
-transmission-gtk obs-studio mkvtoolnix-gui mpv qimgv notepadqq firefox-esr redshift cpu-x udns-utils whois curl
+# Install essential packages (split into multiple lines for readability)
+sudo apt-get install build-essential nano micro nala brightnessctl playerctl \
+dpkg synaptic neofetch htop btop cava xdg-user-dirs xdg-user-dirs-gtk \
+dialog mtools dosfstools gvfs gvfs-backends policykit-1-gnome \
+network-manager network-manager-gnome thunar thunar-archive-plugin thunar-volman \
+file-roller tilix alacritty terminator kitty alacritty pulseaudio alsa-utils \
+pavucontrol volumeicon-alsa pamixer neofetch curl fonts-recommended \
+fonts-ubuntu fonts-font-awesome fonts-terminus picom rofi dunst libnotify-bin \
+unzip zip fonts-font-awesome fonts-ubuntu fonts-liberation2 fonts-liberation \
+fonts-terminus geany geany-plugins pdfarranger numix-icon-theme numix-gtk-theme \
+dmz-cursor-theme intel-microcode flatpak easyssh plymouth transmission-gtk \
+obs-studio mkvtoolnix-gui mpv qimgv notepadqq firefox-esr redshift cpu-x \
+udns-utils whois curl
 
 wait
 
 # Enable Flatpak and add Flathub repository
-sudo systemctl enable flatpak
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo systemctl enable sddm
 
@@ -34,9 +35,10 @@ sudo apt install --no-install-recommends sddm
 # Enable SDDM service to start at boot
 sudo systemctl enable sddm
 
+# Clone and install the Delicious SDDM theme
 git clone https://github.com/stuomas/delicious-sddm-theme.git && cd delicious-sddm-theme && ./install.sh
 
 wait
 
-# reboot
+# Reboot the system
 sudo reboot
