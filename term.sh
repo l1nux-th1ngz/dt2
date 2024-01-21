cb924c5f0a4eb3584a0cad493c6ab121322c0dc8
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-01-20 22:29:16 -0700

    Create term.sh

---
+++ term.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Update the package lists for upgrades and new package installations
+sudo apt-get update
+
+# Install terminator, rofi, and polybar
+sudo apt-get install -y terminator rofi polybar
+
+# Add polybar to startup applications
+echo "@polybar" >> ~/.config/autostart
+
+# Set keyboard shortcuts
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
+"['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', \
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
+
+# Set shortcut for terminator
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Open Terminator'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'terminator'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Primary>Return'
+
+# Set shortcut for kitty
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'Open Kitty'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'kitty'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Primary>t'
+
+# Set shortcut for rofi
+gsettings set org.gnome.mutter.keybindings switch-applications "['<Super>Tab', '<Alt>Tab']"
+gsettings set org.gnome.mutter.keybindings switch-applications-backward "['<Shift><Super>Tab', '<Shift><Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings panel-main-menu "['<Super>s']"
